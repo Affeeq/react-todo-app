2cbf0ff1d8879aae5cc0155d48d17671b8761987
Affeeq Ismail <affeeqabedeenismail@gmail.com>
2021-08-29 17:04:21 -0700

    took out local server and comments

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const router = require("./routes");
 
 // DB_URl = online db url
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/todo";
+const dbUrl = process.env.DB_URL;
 mongoose
   .connect(dbUrl, {
     useNewUrlParser: true,
@@ -26,12 +26,3 @@ app.listen(process.env.PORT || 8000, () => {
 	console.log(`Server running on port ${process.env.PORT || 8000}`);
 });
 module.exports = app;
-
-
-// const uri = "mongodb+srv://Affeeq:<password>@cluster0.qkdor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
