ffcd8fd5583949c717414b1274e2671940eb0473
Affeeq Ismail <affeeqabedeenismail@gmail.com>
2021-08-29 19:11:20 -0700

    add app listen to port

--- index.js
+++ index.js
@@ -39,5 +39,8 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+app.listen(process.env.PORT || 8000, () => {
+	console.log(`Server running on port ${process.env.PORT || 8000}`);
+});
 
 module.exports = app;
